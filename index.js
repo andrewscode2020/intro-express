349e4bb27ce185fb5d26252e5ad193fe9c9bae3f
andrewscode2020 <greatandres@gmail.com>
2020-11-05 11:51:55 -0500

    segundo commit

--- index.js
+++ index.js
@@ -1,9 +1,26 @@
 const express = require('express')
 const app = express()
 
-app.get('/', (solicitud, respuesta) => {
-    respuesta.send('<h1>Hola</h1>')
+app.get('/clientes', (solicitud, respuesta) => {
+    respuesta.send(['arreglo_clientes'])
 })
+
+app.get('/libros', (solicitud, respuesta) => {
+    respuesta.send(['arreglo_libros'])
+})
+
+app.get('/precios', (solicitud, respuesta) => {
+    respuesta.send(['arreglo_precios'])
+})
+
+
+// app.post('/', (solicitud, respuesta) => {
+//     respuesta.send({error: 'No soportamos que preguntes usando el método POST'})
+// })
+
+// app.delete('/', (solicitud, respuesta) => {
+//     respuesta.send({error: 'No podrás eliminar nada'})
+// })
 
 // Encender el servidor 
 // app.listen(número de puerto, funcion para saber si el servidor ya está encendido):
